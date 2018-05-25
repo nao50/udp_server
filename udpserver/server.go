9c8c70344f2c73efb912a7fbf629fd8f2ce4757d
naoyamaguchi <naoyamaguchi0128@gmail.com>
2018-05-25 16:58:21 +0900

    edit

--- udpserver/server.go
+++ udpserver/server.go
@@ -54,3 +54,93 @@ func main() {
 // 		log.Println("End test. ", time.Since(start))
 // 	}
 // }
+func uplink() {
+	uplinkBuffer := make([]byte, 1550)
+	ctx, limit := tbf.InitTokenBucket()
+	///////////////////////////////////////////////////////////////	
+	sendUdpAddr := &net.UDPAddr{
+		IP:   net.ParseIP("10.0.11.10"),
+		Port: 2153,
+	}
+	// sendUdpConn, err := net.ListenUDP("udp", sendUdpAddr)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	///////////////////////////////////////////////////////////////
+	recvUdpAddr := &net.UDPAddr{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: 2152,
+	}
+	recvUdpConn, err := net.ListenUDP("udp", recvUdpAddr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	///////////////////////////////////////////////////////////////
+	for {
+		n, addr, err := recvUdpConn.ReadFromUDP(uplinkBuffer)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// tbf
+		err = tbf.TokenBucketFilter(ctx, n, limit)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		go func() {
+			// recvUdpConn.WriteTo(buf[:n], addr)
+			recvUdpConn.WriteTo(uplinkBuffer[:n], sendUdpAddr)
+			// sendUdpConn.WriteTo(uplinkBuffer[:n], sendUdpAddr)
+		}()
+
+	}
+
+}
+
+
+
+func downlink() {
+	uplinkBuffer := make([]byte, 1550)
+	ctx, limit := tbf.InitTokenBucket()
+	///////////////////////////////////////////////////////////////	
+	sendUdpAddr := &net.UDPAddr{
+		IP:   net.ParseIP("10.0.10.10"),
+		Port: 2253,
+	}
+	// sendUdpConn, err := net.ListenUDP("udp", sendUdpAddr)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	///////////////////////////////////////////////////////////////
+	recvUdpAddr := &net.UDPAddr{
+		IP:   net.ParseIP("0.0.0.0"),
+		Port: 2252,
+	}
+	recvUdpConn, err := net.ListenUDP("udp", recvUdpAddr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	///////////////////////////////////////////////////////////////
+	for {
+		n, addr, err := recvUdpConn.ReadFromUDP(uplinkBuffer)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// tbf
+		err = tbf.TokenBucketFilter(ctx, n, limit)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		go func() {
+			// recvUdpConn.WriteTo(buf[:n], addr)
+			recvUdpConn.WriteTo(uplinkBuffer[:n], sendUdpAddr)
+			// sendUdpConn.WriteTo(uplinkBuffer[:n], sendUdpAddr)
+		}()
+
+	}
+
+}
+
